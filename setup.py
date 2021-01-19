4d437016c56fab20d59f4717f4aba80f5dbb479c
cyderbot97 <49147296+cyderbot97@users.noreply.github.com>
2021-01-19 09:39:33 +0100

    word
    ajout de word, attention modification de la librairie docx2pdf pour enlever le pbar sinon probleme avec cx_freeze.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-exe = [Executable("fusion.py", base = "Win32GUI")]
-setup(
-    name = "salut",
-    version = "0.1",
-    description = "Ce programme vous dit bonjour",
-    executables = exe,
-)
